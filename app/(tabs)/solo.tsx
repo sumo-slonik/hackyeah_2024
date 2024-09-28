60309cfc9d9168ca72768656d65e964ff43745cc
war-in <war-in@users.noreply.github.com>
2024-09-28 22:57:13 +0200

    fix solo page

--- app/(tabs)/solo.tsx
+++ app/(tabs)/solo.tsx
@@ -1,15 +1,10 @@
-import { Text } from 'react-native';
 import { UserProfil } from '@/components/custom_components/userProfil';
 import { firstUser } from '@/assets/customData/personalData';
-import { VStack } from 'native-base';
-import TimelineCalendarScreen from '@/components/WeekCalendar';
+import { View } from 'react-native';
 
 export default function Solo() {
     return (
-        <VStack flex={1}>
-            <TimelineCalendarScreen />
-            // add to proper place
-            <UserProfil user={firstUser}></UserProfil>
-        </VStack>
+        // add to proper place
+        <UserProfil user={firstUser}></UserProfil>
     );
 }
