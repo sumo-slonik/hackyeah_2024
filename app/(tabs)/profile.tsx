21a3e8b264a47c862c2f82865117b2b3e7a1aca6
sumo-slonik <jakobn@o2.pl>
2024-09-28 22:43:09 +0200

    working main site

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,50 +1,137 @@
-import {VStack, Box, Text, Progress, HStack, IconButton, Icon} from 'native-base';
+import {VStack, Box, Text, Progress, HStack, IconButton, Icon, Button, Center, Flex, Image} from 'native-base';
 
-import {Entypo} from "@expo/vector-icons";
-
+import {FontAwesome} from "@expo/vector-icons";
+const styles = {
+    box:{
+        marginBottom: 10
+    }
+}
 export default function Profile() {
     return (
-
-
         <>
-
-            <VStack space={4} alignItems="center">
-                <Box bg="primary.500" p={4} rounded="md">
+            <VStack space={4} alignItems="right" style={styles.box}>
+                <HStack space={4} justifyContent="left" alignItems="left">
+                    <Text >YOU</Text>
+                    <IconButton
+                        icon={<Icon as={FontAwesome} name="arrow-right"/>}
+                        _icon={{
+                            color: "white",
+                            size: "md",
+                        }}
+                        _hover={{
+                            bg: "primary.600",
+                        }}
+                        _pressed={{
+                            bg: "primary.800",
+                        }}
+                        bg="primary.500"
+                    />
+                </HStack>
+                <Box bg="primary.500" p={4} rounded="md" justifyContent="right" alignItems="right">
                     <Text color="white">YOUR DAILY GOALS</Text>
                     <Progress bg="coolGray.100" _filledTrack={{
                         bg: "lime.500"
                     }} value={75} mx="4"/>
+                    <Box bg="primary.500" p={4} rounded="md" justifyContent="center" alignItems="center">
+                        <Text color="white">1950/2400</Text>
+                    </Box>
                 </Box>
                 <Box bg="secondary.500" p={4} rounded="md">
-                    <HStack space={4} justifyContent="center" alignItems="center">
-                        <Box bg="primary.500" p={4} rounded="md">
-                            <Text color="white">GAIN POINTS</Text>
-                        </Box>
-                        <Box bg="secondary.500" p={4} rounded="md">
-                            <IconButton icon={<Icon as={Entypo} name="arrow-forward" />} borderRadius="full"/>
-                        </Box>
-                        <Box bg="emerald.500" p={4} rounded="md">
-                            <Text color="white">Komponent 3</Text>
-                        </Box>
+                    <HStack space={4} justifyContent="left" alignItems="center">
+                        <Text color="white">GAIN POINTS</Text>
+                        <IconButton
+                            icon={<Icon as={FontAwesome} name="arrow-right"/>}
+                            _icon={{
+                                color: "white",
+                                size: "md",
+                            }}
+                            _hover={{
+                                bg: "primary.600",
+                            }}
+                            _pressed={{
+                                bg: "primary.800",
+                            }}
+                            bg="primary.500"
+                        />
                     </HStack>
                 </Box>
-                <Box bg="emerald.500" p={4} rounded="md">
-                    <Text color="white">Komponent 3</Text>
+                <Box bg="secondary.500" p={4} rounded="md">
+                    <Text color="white">Upcoming activities</Text>
+                    <VStack>
+                        <Box bg={"primary.600"} style={styles.box}>
+                            <HStack space={4} justifyContent="left" alignItems="center">
+                                <Text color="white">Tenis with Iga</Text>
+                                <IconButton
+                                    icon={<Icon as={FontAwesome} name="arrow-right"/>}
+                                    _icon={{
+                                        color: "white",
+                                        size: "md",
+                                    }}
+                                    _hover={{
+                                        bg: "primary.600",
+                                    }}
+                                    _pressed={{
+                                        bg: "primary.800",
+                                    }}
+                                    bg="primary.500"
+                                />
+                            </HStack>
+                        </Box>
+                        <Box bg={"primary.600"}>
+                            <HStack space={4} justifyContent="left" alignItems="center">
+                                <Text color="white">Footbal with Robercik</Text>
+                                <IconButton
+                                    icon={<Icon as={FontAwesome} name="arrow-right"/>}
+                                    _icon={{
+                                        color: "white",
+                                        size: "md",
+                                    }}
+                                    _hover={{
+                                        bg: "primary.600",
+                                    }}
+                                    _pressed={{
+                                        bg: "primary.800",
+                                    }}
+                                    bg="primary.500"
+                                />
+                            </HStack>
+                        </Box>
+                    </VStack>
+
                 </Box>
             </VStack>
-
-            {/*    <span>*/}
-            {/*    YOUR DAILY GOALS*/}
-            {/*    </span>*/}
-            {/*        <Center w="100%">*/}
-            {/*            <Box w="90%" maxW="400">*/}
-
-            {/*            </Box>*/}
-            {/*        </Center>*/}
-
-            {/*<Box alignItems="center">*/}
-
-            {/*</Box>*/}
+            <Box alignItems="center" alignContent="center">
+            <Box
+                bg="blue.800"
+                height={200}
+                width={300}
+                position="relative"
+                borderRadius="md"
+                overflow="hidden"
+            >
+                <Image
+                    source={require('../../assets/images/sample_galaxy.jpeg')}
+                    alt="Opis obrazka"
+                    size="full"
+                    resizeMode="cover"
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    right={0}
+                    bottom={0}
+                />
+                <Button
+                    position="absolute"
+                    bottom={2}
+                    right={4}
+                    borderRadius="full"
+                    bg="primary.500"
+                    _text={{color: "white"}}
+                >
+                    Edit
+                </Button>
+            </Box>
+            </Box>
         </>
     );
 }
