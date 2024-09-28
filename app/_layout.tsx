f7d849c526973415bada3d56756a631a261607c6
Adam Nowak <anowak2000@gmail.com>
2024-09-28 19:21:47 +0200

    added safe area view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,7 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { NativeBaseProvider } from 'native-base';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -36,13 +37,15 @@ export default function RootLayout() {
             value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
         >
             <NativeBaseProvider>
-                <Stack>
-                    <Stack.Screen
-                        name="(tabs)"
-                        options={{ headerShown: false }}
-                    />
-                    <Stack.Screen name="+not-found" />
-                </Stack>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <Stack>
+                        <Stack.Screen
+                            name="(tabs)"
+                            options={{ headerShown: false }}
+                        />
+                        <Stack.Screen name="+not-found" />
+                    </Stack>
+                </SafeAreaView>
             </NativeBaseProvider>
         </ThemeProvider>
     );
