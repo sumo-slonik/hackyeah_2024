c5f6230264dd7dd49315d662b37fe0b13b8cf4aa
MaciekBartoszNowakowski <macieknowakowski556@gmail.com>
2024-09-29 00:55:14 +0200

    changed description to english

--- assets/customData/personalData.tsx
+++ assets/customData/personalData.tsx
@@ -2,10 +2,10 @@ export const firstUser = {
     name: 'Maciek',
     age: '20',
     data: [
-        { label: 'punktualność', value: 30 },
-        { label: 'ogólna atmosfera', value: 55 },
-        { label: 'higiena', value: 60 },
-        { label: 'zgodność z opisem', value: 70 },
+        { label: 'punctuality', value: 30 },
+        { label: 'general atmosphere', value: 55 },
+        { label: 'hygiene', value: 60 },
+        { label: 'compliance with the description', value: 70 },
     ],
     photo: require('@/assets/images/samples_galaxy.jpeg'),
 };
