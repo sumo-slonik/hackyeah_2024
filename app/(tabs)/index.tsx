081deebbe06a1c4e0b498df8830415b896a55c4f
sumo-slonik <jakobn@o2.pl>
2024-09-29 06:08:42 +0200

    finish work with task

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,6 +11,7 @@ import {
     Flex,
     Image,
     Spacer,
+    ScrollView,
 } from 'native-base';
 
 import { FontAwesome } from '@expo/vector-icons';
@@ -22,6 +23,7 @@ import {
     expaningRow,
 } from '@/components/custom_components/ExpaningRow';
 import { UpcomingActivity } from '@/components/custom_components/UpcomingActivity';
+import Header from '@/components/Header';
 
 const styles = {
     box: {
@@ -31,132 +33,135 @@ const styles = {
 export default function Index() {
     return (
         <>
-            <Box bg={Colors.light.darkBackGround}>
-                <VStack space={4} alignItems="right" style={styles.box}>
-                    <HStack
-                        space={4}
-                        justifyContent="space-between"
-                        alignItems="center"
-                        w="100%"
-                    >
-                        <Text fontSize={32} color={Colors.light.white}>
-                            YOU
-                        </Text>
-                        <Spacer />
-                        <Link href="/user">
-                            <View>
-                                <Text fontSize={32} color={Colors.light.white}>
-                                    Go to user
-                                </Text>
-                            </View>
-                        </Link>
-                    </HStack>
-                    <Box
-                        bg={Colors.light.darkColor3}
-                        p={4}
-                        rounded="md"
-                        justifyContent="right"
-                        alignItems="right"
-                    >
-                        <Text color="white">YOUR DAILY GOALS</Text>
-                        <Progress
-                            bg={Colors.light.darkBackGround}
-                            borderRadius={0}
-                            borderColor={Colors.light.white}
-                            borderWidth={1}
-                            height={3}
-                            _filledTrack={{
-                                bg: Colors.light.lightColor1,
-                                borderRadius: 0,
-                            }}
-                            value={75}
-                            mx="4"
-                        />
+            <ScrollView>
+                <Box bg={Colors.light.darkBackGround}>
+                    <Header title={'YOU'} />
+
+                    <VStack space={4} alignItems="right" style={styles.box}>
                         <Box
+                            bg={Colors.light.darkColor3}
                             p={4}
                             rounded="md"
-                            justifyContent="left"
-                            alignItems="left"
+                            justifyContent="right"
+                            alignItems="right"
                         >
-                            <Text color={Colors.light.lightColor2}>
-                                1950/2400 ENERGY
-                            </Text>
+                            <Text color="white">YOUR DAILY GOALS</Text>
+                            <Progress
+                                bg={Colors.light.darkBackGround}
+                                borderRadius={0}
+                                borderColor={Colors.light.white}
+                                borderWidth={1}
+                                height={3}
+                                _filledTrack={{
+                                    bg: Colors.light.lightColor1,
+                                    borderRadius: 0,
+                                }}
+                                value={75}
+                                mx="4"
+                            />
+                            <Box
+                                p={4}
+                                rounded="md"
+                                justifyContent="left"
+                                alignItems="left"
+                            >
+                                <Text color={Colors.light.lightColor2}>
+                                    1950/2400 ENERGY
+                                </Text>
+                            </Box>
+                        </Box>
+                        <Box bg={Colors.light.darkColor3} p={4} rounded="md">
+                            <HStack
+                                space={4}
+                                justifyContent="left"
+                                alignItems="center"
+                            >
+                                <Text color="white">GAIN more ENERGY</Text>
+                                <Link href="/solo">
+                                    <View>
+                                        <Text>Go to solo</Text>
+                                    </View>
+                                </Link>
+                            </HStack>
+                        </Box>
+                        <Box bg={Colors.light.darkColor3} p={4} rounded="md">
+                            <Text color="white">Upcoming activities</Text>
+                            <VStack>
+                                <ExpaningRow title={'Tenis with Iga'}>
+                                    <UpcomingActivity
+                                        dateTime={'21-05-2037'}
+                                        activityType={'Tenis - pro activity'}
+                                    />
+                                </ExpaningRow>
+
+                                <ExpaningRow title={'Football with Robercik'}>
+                                    <UpcomingActivity
+                                        dateTime={'21-05-2037'}
+                                        activityType={'Tenis - pro activity'}
+                                    />
+                                </ExpaningRow>
+
+                                <ExpaningRow title={'Baciata with Ania'}>
+                                    <UpcomingActivity
+                                        dateTime={'21-05-2037'}
+                                        activityType={'Tenis - pro activity'}
+                                    />
+                                </ExpaningRow>
+                            </VStack>
+                            <HStack
+                                justifyContent="flex-end" // Wyrównanie do prawej strony
+                                width="100%" // Zajmuje całą szerokość wiersza
+                            >
+                                <IconButton
+                                    backgroundColor={Colors.light.darkColor2}
+                                    icon={
+                                        <Icon
+                                            as={FontAwesome}
+                                            name={'calendar'}
+                                        />
+                                    }
+                                    _icon={{
+                                        color: Colors.light.lightColor1,
+                                        size: 'md',
+                                    }}
+                                />
+                            </HStack>
+                        </Box>
+                    </VStack>
+
+                    <Box alignItems="center" alignContent="center">
+                        <Box
+                            bg="blue.800"
+                            height={200}
+                            width={300}
+                            position="relative"
+                            borderRadius="md"
+                            overflow="hidden"
+                        >
+                            <Image
+                                source={require('../../assets/images/sample_galaxy.jpeg')}
+                                alt="Opis obrazka"
+                                size="full"
+                                resizeMode="cover"
+                                position="absolute"
+                                top={0}
+                                left={0}
+                                right={0}
+                                bottom={0}
+                            />
+                            <Button
+                                position="absolute"
+                                bottom={2}
+                                right={4}
+                                bg={Colors.light.lightColor1}
+                                _text={{ color: Colors.light.darkBackGround }}
+                            >
+                                Edit
+                            </Button>
                         </Box>
                     </Box>
-                    <Box bg={Colors.light.darkColor3} p={4} rounded="md">
-                        <HStack
-                            space={4}
-                            justifyContent="left"
-                            alignItems="center"
-                        >
-                            <Text color="white">GAIN more ENERGY</Text>
-                            <Link href="/solo">
-                                <View>
-                                    <Text>Go to solo</Text>
-                                </View>
-                            </Link>
-                        </HStack>
-                    </Box>
-                    <Box bg={Colors.light.darkColor3} p={4} rounded="md">
-                        <Text color="white">Upcoming activities</Text>
-                        <VStack>
-                            <ExpaningRow title={'Tenis with Iga'}>
-                                <UpcomingActivity
-                                    dateTime={'21-05-2037'}
-                                    activityType={'Tenis - pro activity'}
-                                />
-                            </ExpaningRow>
-
-                            <ExpaningRow title={'Football with Robercik'}>
-                                <UpcomingActivity
-                                    dateTime={'21-05-2037'}
-                                    activityType={'Tenis - pro activity'}
-                                />
-                            </ExpaningRow>
-
-                            <ExpaningRow title={'Baciata with Ania'}>
-                                <UpcomingActivity
-                                    dateTime={'21-05-2037'}
-                                    activityType={'Tenis - pro activity'}
-                                />
-                            </ExpaningRow>
-                        </VStack>
-                    </Box>
-                </VStack>
-
-                <Box alignItems="center" alignContent="center">
-                    <Box
-                        bg="blue.800"
-                        height={200}
-                        width={300}
-                        position="relative"
-                        borderRadius="md"
-                        overflow="hidden"
-                    >
-                        <Image
-                            source={require('../../assets/images/sample_galaxy.jpeg')}
-                            alt="Opis obrazka"
-                            size="full"
-                            resizeMode="cover"
-                            position="absolute"
-                            top={0}
-                            left={0}
-                            right={0}
-                            bottom={0}
-                        />
-                        <Button
-                            position="absolute"
-                            bottom={2}
-                            right={4}
-                            borderRadius="full"
-                            bg="primary.500"
-                            _text={{ color: 'white' }}
-                        >
-                            Edit
-                        </Button>
-                    </Box>
                 </Box>
-            </Box>
+            </ScrollView>
         </>
     );
 }
