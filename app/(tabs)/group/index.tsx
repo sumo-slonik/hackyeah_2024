4cbc18a2e5aeb765477aa16bc465ea660843b916
chrisgadek1 <krzysztof.gadek@zendesk.com>
2024-09-29 04:36:05 +0200

    template

--- app/(tabs)/group/index.tsx
+++ app/(tabs)/group/index.tsx
@@ -1,18 +1,12 @@
+import { Dimensions, SafeAreaView, Text, View } from 'react-native';
 import {
-    Dimensions,
-    SafeAreaView,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
-import {
-    Button,
+    Button, Center,
     Fab,
-    FlatList,
-    Icon,
+    FlatList, HStack,
+    Icon, Input,
     Pressable,
     ScrollView,
-    Select,
+    Select
 } from 'native-base';
 import { useState } from 'react';
 import MapPicker from '@/components/MapPicker';
@@ -21,14 +15,15 @@ import { Link } from 'expo-router';
 import { MaterialIcons } from '@expo/vector-icons';
 import WeekCalendar from '@/components/WeekCalendar';
 import { LatLng } from 'react-native-maps';
-import { useThemeColor } from '@/hooks/useThemeColor';
+import ActivityCard from '@/components/group/ActivityCard';
+import { ActivityIcons } from '@/constants/ActivityIcons';
 
 export default function Group() {
     const [selectedActivity, setSelectedActivity] = useState<string>('');
 
     const data = [
         {
-            name: 'Dancing',
+            name: 'Tennis',
         },
         {
             name: 'Running',
@@ -40,9 +35,15 @@ export default function Group() {
             name: 'Swimming',
         },
         {
-            name: 'Skateboarding',
+            name: 'Cycling',
         },
     ];
+
+    const activitiesRow = (activities: string[], marginBottom: number) => (
+        <HStack space={5} style={{ marginBottom: marginBottom }}>
+            {activities.map(activity => <ActivityCard color={"#3C096C"} fontSize={16} fontColor={"#FFFFFF"} activity={activity} width={108} height={73} />)}
+        </HStack>
+    )
 
     const renderItem = (item: { name: any }) => {
         return (
@@ -58,43 +59,41 @@ export default function Group() {
 
     return (
         <ScrollView>
-            <Header title={'Multi'} />
-            <Text
-                style={{
-                    fontSize: 24,
-                    color: useThemeColor({}, 'text'),
-                    paddingHorizontal: 4,
-                    paddingVertical: 12,
-                }}
-            >
+            <Header title={'MULTI'} />
+            <Text style={{ fontSize: 24, marginLeft: 20 }}>
+                Currently Synergizing for:
+            </Text>
+            <ScrollView horizontal scrollEnabled style={{ margin: 20 }}>
+                <HStack space={5}>
+                    {data.map((item) => <ActivityCard fontSize={24} color={"#3C096C"} fontColor={"#FFFFFF"} activity={item.name} width={128} height={86} />)}
+                </HStack>
+            </ScrollView>
+            <Text style={{ fontSize: 24, marginLeft: 20 }}>
                 Hot/Last Activities In Your Area
             </Text>
-            <ScrollView horizontal scrollEnabled>
-                {data.map((item) => renderItem(item))}
+            <ScrollView horizontal scrollEnabled style={{ margin: 20 }}>
+                <HStack space={5}>
+                    {data.map((item) => <ActivityCard fontSize={24} color={"#FF6D00"} fontColor={"#240046"} activity={item.name} width={128} height={86} />)}
+                </HStack>
             </ScrollView>
-
-            <Link
-                href={{
-                    pathname: '/group/chooseLocationAndTime',
-                    params: {
-                        activity: selectedActivity,
-                    },
-                }}
-                asChild
-            >
-                <Fab
-                    renderInPortal={false}
-                    shadow={2}
-                    size="lg"
-                    icon={
-                        <Icon
-                            as={MaterialIcons}
-                            color="white"
-                            name="arrow-forward"
-                        />
-                    }
-                />
-            </Link>
+            <Text style={{ fontSize: 24, marginLeft: 20 }}>
+                All Activities
+            </Text>
+            <Center style={{ marginTop: 20, marginBottom: 20 }}>
+                <HStack space={2} flexDirection="row" width={"90%"}>
+                    <Input flexGrow={100} placeholder={"Search"} fontSize={24} />
+                    <Button bgColor={"#FF9E00"} flexGrow={16} >DUO</Button>
+                    <Button bgColor={"#FF9E00"} flexGrow={23}>GROUP</Button>
+                </HStack>
+            </Center>
+            <ScrollView scrollEnabled style={{ marginLeft: 20 }}>
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(0, 3), 10) }
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(3, 6), 10) }
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(6, 9), 10) }
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(9, 12), 10) }
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(12, 15), 10) }
+                { activitiesRow(Object.keys(ActivityIcons["sports"]).slice(15, 18), 10) }
+            </ScrollView>
         </ScrollView>
     );
 }
